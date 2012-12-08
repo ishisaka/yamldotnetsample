f1abb38bbf168e4dbbe8354727cf12d9da2d63f5
Tadahiro Ishisaka <ishisaka@gmail.com>
2012-12-08 15:39:00 +0900

    FirstCommit

---
+++ CsAdventCalendarDec9/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.RepresentationModel.Serialization;
+
+namespace CsAdventCalendarDec9
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            //シリアライズするオブジェクトを定義する
+            var calendar = new AdventCalendar();
+            calendar.Year = new DateTime(2012, 12, 1);
+            calendar.Description = "C# Advent Calender 2012";
+            calendar.Contents.Add(new Content()  {
+                Day = 1,
+                Author = "gushwell",
+                Description = ".NET Framework4.5 での非同期I/O処理 事始め",
+                URL = @"http://gushwell.ldblog.jp/archives/52290230.html"
+            });
+            calendar.Contents.Add(new Content() {
+                Day = 2,
+                Author = "KTZ",
+                Description = "年末の大掃除",
+                URL = @"http://ritalin.github.com/2012/12-02/csharp-advent-2012/"
+            });
+            calendar.Contents.Add(new Content() {
+                Day = 3,
+                Author = "neuecc",
+                Description = "MemcachedTranscoder – C#のMemcached用シリアライザライブラリ",
+                URL = @"http://neue.cc/2012/12/03_389.html"
+            });
+
+            //YAMLにシリアライズしてコンソールに表示
+            var serializer = new Serializer();
+            serializer.Serialize(Console.Out, calendar);
+
+            Console.Read();
+        }
+
+    }
+
+    public class Content
+    {
+        public int Day { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set;}   
+
+    }
+
+
+    public class AdventCalendar
+    {
+        public DateTime Year { get; set; }
+        public string Description { get; set; }
+        public List<Content> Contents { get; set; }
+
+        public AdventCalendar() {
+            this.Contents = new List<Content>();
+        }
+
+        public AdventCalendar(List<Content> contents) {
+            this.Contents = contents;
+        }
+    }
+}
---
+++ CsAdventCalendarDec9DeSerialize/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using YamlDotNet;
+using YamlDotNet.RepresentationModel;
+using YamlDotNet.RepresentationModel.Serialization;
+
+
+namespace CsAdventCalendarDec9DeSerialize
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            // YAMLのファイルをUTF-8エンコーディングされたファイルとして読み込む
+            var input = new StreamReader("AdventCalendar.yaml", Encoding.UTF8);
+            // YAMLのストリームを解析する
+            var yaml = new YamlStream();
+            yaml.Load(input);
+            // ストリームを解析する
+            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+
+            //Yearノードの値を取得する
+            var Year = (YamlScalarNode)mapping.Children[new YamlScalarNode("Year")];
+            Console.WriteLine("Year\t{0}", Year.Value);
+            //Descriptionノードの値を表示する
+            var Description = (YamlScalarNode)mapping.Children[new YamlScalarNode("Description")];
+            Console.WriteLine("Desciption\t{0}", Description.Value);
+            //Contents以下の内容表示
+            Console.WriteLine("Contents:");
+            //シーケンス(配列)のノードとしてContentsノードの内容を取り出す
+            var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("Contents")];
+            //Contentsの各Contentをマッピングノードとして取り出す
+            foreach (YamlMappingNode item in items) {
+                //Contentの個別要素を表示させる
+                foreach (var child in item) {
+                    Console.WriteLine(
+                        "{0}\t{1}", 
+                        ((YamlScalarNode)child.Key).Value, 
+                        ((YamlScalarNode)child.Value).Value);
+                }
+            }
+
+            // クラスに直接デシリアライズする。
+            //YAMLファイルを開き直す
+            input.Close();
+            input = new StreamReader("AdventCalendar.yaml", Encoding.UTF8);
+            //YAMLファイルからContentsオブジェクトに復元するためのYamlSerializer(T)クラスのインスタンスを作る
+            //Genericで復元先のクラスを指定する
+            var yamlSerializer = new YamlSerializer<AdventCalendar>();
+            //C#nのオブジェクトに復元を行う
+            var calendar = yamlSerializer.Deserialize(input);
+            //復元した結果を表示する。
+            Console.WriteLine("Year:{0}", calendar.Year);
+            Console.WriteLine("Description:{0}", calendar.Description);
+            foreach (var content in calendar.Contents) {
+                Console.WriteLine("\tDay:{0}", content.Day);
+                Console.WriteLine("\tAuthor:{0}", content.Author);
+                Console.WriteLine("\tDescription:{0}", content.Description);
+                Console.WriteLine("\tURL:{0}", content.URL);
+            }
+
+            input.Close();
+            Console.Read();
+        }
+    }
+
+    public class Content
+    {
+        public int Day { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string URL { get; set; }
+
+    }
+
+
+    public class AdventCalendar
+    {
+        public DateTime Year { get; set; }
+        public string Description { get; set; }
+        public List<Content> Contents { get; set; }
+
+        public AdventCalendar() {
+            this.Contents = new List<Content>();
+        }
+
+        public AdventCalendar(List<Content> contents) {
+            this.Contents = contents;
+        }
+    }
+
+}
